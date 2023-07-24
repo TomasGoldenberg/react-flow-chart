bb64ac535cc2a65dbc002fe7c7b21f7a10a986f2
TomasGoldenberg <soytomasgoldenberg@gmail.com>
2023-07-24 11:46:29 -0700

    functional

--- src/components/Node/Node.wrapper.tsx
+++ src/components/Node/Node.wrapper.tsx
@@ -1,44 +1,59 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import Draggable, { DraggableData } from 'react-draggable'
-import ResizeObserver from 'react-resize-observer'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Draggable, { DraggableData } from "react-draggable";
+import ResizeObserver from "react-resize-observer";
 import {
-  IConfig, ILink, INode, INodeInnerDefaultProps, IOnDragNode,
-  IOnDragNodeStop, IOnLinkCancel,
-  IOnLinkComplete, IOnLinkMove,
-  IOnLinkStart, IOnNodeClick, IOnNodeDoubleClick, IOnNodeMouseEnter,
-  IOnNodeMouseLeave, IOnNodeSizeChange, IOnPortPositionChange,
-  IPortDefaultProps, IPortsDefaultProps,
-  IPosition, ISelectedOrHovered, ISize, PortWrapper,
-} from '../../'
-import { noop } from '../../utils'
-import { INodeDefaultProps, NodeDefault } from './Node.default'
+  IConfig,
+  ILink,
+  INode,
+  INodeInnerDefaultProps,
+  IOnDragNode,
+  IOnDragNodeStop,
+  IOnLinkCancel,
+  IOnLinkComplete,
+  IOnLinkMove,
+  IOnLinkStart,
+  IOnNodeClick,
+  IOnNodeDoubleClick,
+  IOnNodeMouseEnter,
+  IOnNodeMouseLeave,
+  IOnNodeSizeChange,
+  IOnPortPositionChange,
+  IPortDefaultProps,
+  IPortsDefaultProps,
+  IPosition,
+  ISelectedOrHovered,
+  ISize,
+  PortWrapper,
+} from "../../";
+import { noop } from "../../utils";
+import { INodeDefaultProps, NodeDefault } from "./Node.default";
 
 export interface INodeWrapperProps {
-  config: IConfig
-  node: INode
-  Component: React.FunctionComponent<INodeDefaultProps>
-  offset: IPosition
-  selected: ISelectedOrHovered | undefined
-  hovered: ISelectedOrHovered | undefined
-  selectedLink: ILink | undefined
-  hoveredLink: ILink | undefined
-  isSelected: boolean
-  NodeInner: React.FunctionComponent<INodeInnerDefaultProps>
-  Ports: React.FunctionComponent<IPortsDefaultProps>
-  Port: React.FunctionComponent<IPortDefaultProps>
-  onPortPositionChange: IOnPortPositionChange
-  onLinkStart: IOnLinkStart
-  onLinkMove: IOnLinkMove
-  onLinkComplete: IOnLinkComplete
-  onLinkCancel: IOnLinkCancel
-  onDragNode: IOnDragNode
-  onDragNodeStop: IOnDragNodeStop
-  onNodeClick: IOnNodeClick
-  onNodeDoubleClick: IOnNodeDoubleClick
-  onNodeSizeChange: IOnNodeSizeChange
-  onNodeMouseEnter: IOnNodeMouseEnter
-  onNodeMouseLeave: IOnNodeMouseLeave
+  config: IConfig;
+  node: INode;
+  Component: React.FunctionComponent<INodeDefaultProps>;
+  offset: IPosition;
+  selected: ISelectedOrHovered | undefined;
+  hovered: ISelectedOrHovered | undefined;
+  selectedLink: ILink | undefined;
+  hoveredLink: ILink | undefined;
+  isSelected: boolean;
+  NodeInner: React.FunctionComponent<INodeInnerDefaultProps>;
+  Ports: React.FunctionComponent<IPortsDefaultProps>;
+  Port: React.FunctionComponent<IPortDefaultProps>;
+  onPortPositionChange: IOnPortPositionChange;
+  onLinkStart: IOnLinkStart;
+  onLinkMove: IOnLinkMove;
+  onLinkComplete: IOnLinkComplete;
+  onLinkCancel: IOnLinkCancel;
+  onDragNode: IOnDragNode;
+  onDragNodeStop: IOnDragNodeStop;
+  onNodeClick: IOnNodeClick;
+  onNodeDoubleClick: IOnNodeDoubleClick;
+  onNodeSizeChange: IOnNodeSizeChange;
+  onNodeMouseEnter: IOnNodeMouseEnter;
+  onNodeMouseLeave: IOnNodeMouseLeave;
 }
 
 export const NodeWrapper = ({
@@ -67,79 +82,91 @@ export const NodeWrapper = ({
   onLinkComplete,
   onLinkCancel,
 }: INodeWrapperProps) => {
-  const [size, setSize] = React.useState<ISize>({ width: 0, height: 0 })
+  const [size, setSize] = React.useState<ISize>({ width: 0, height: 0 });
 
-  const isDragging = React.useRef(false)
+  const isDragging = React.useRef(false);
 
   const onStart = React.useCallback((e: MouseEvent) => {
     // Stop propagation so the canvas does not move
-    e.stopPropagation()
-    isDragging.current = false
-  },[])
+    e.stopPropagation();
+    isDragging.current = false;
+  }, []);
 
-  const onDrag = React.useCallback((event: MouseEvent, data: DraggableData) => {
-    isDragging.current = true
-    onDragNode({ config, event, data, id: node.id })
-  }, [onDragNode, config, node.id])
+  const onDrag = React.useCallback(
+    (event: MouseEvent, data: DraggableData) => {
+      isDragging.current = true;
+      onDragNode({ config, event, data, id: node.id });
+    },
+    [onDragNode, config, node.id]
+  );
 
-  const onStop = React.useCallback((event: MouseEvent, data: DraggableData) => {
-    onDragNodeStop({ config, event, data, id: node.id })
-  }, [onDragNodeStop, config, node.id])
+  const onStop = React.useCallback(
+    (event: MouseEvent, data: DraggableData) => {
+      onDragNodeStop({ config, event, data, id: node.id });
+    },
+    [onDragNodeStop, config, node.id]
+  );
 
-  const onClick = React.useCallback((e: React.MouseEvent) => {
-    if (!config.readonly) {
-      e.stopPropagation()
-      if (!isDragging.current) {
-        onNodeClick({ config, nodeId: node.id })
+  const onClick = React.useCallback(
+    (e: React.MouseEvent) => {
+      if (!config.readonly) {
+        e.stopPropagation();
+        if (!isDragging.current) {
+          onNodeClick({ config, nodeId: node.id });
+        }
       }
-    }
-  }, [config, node.id])
+    },
+    [config, node.id]
+  );
 
-  const onDoubleClick = React.useCallback((e: React.MouseEvent) => {
-    if (!config.readonly) {
-      e.stopPropagation()
-      if (!isDragging.current) {
-        onNodeDoubleClick({ config, nodeId: node.id })
+  const onDoubleClick = React.useCallback(
+    (e: React.MouseEvent) => {
+      if (!config.readonly) {
+        e.stopPropagation();
+        if (!isDragging.current) {
+          onNodeDoubleClick({ config, nodeId: node.id });
+        }
       }
-    }
-  }, [config, node.id])
+    },
+    [config, node.id]
+  );
 
-  const onMouseEnter = React.useCallback(() => {
-    onNodeMouseEnter({ config, nodeId: node.id })
-  }, [config, node.id])
+  /*   const onMouseEnter = React.useCallback(() => {
+    onNodeMouseEnter({ config, nodeId: node.id });
+  }, [config, node.id]); */
 
   const onMouseLeave = React.useCallback(() => {
-    onNodeMouseLeave({ config, nodeId: node.id })
-  }, [config, node.id])
+    onNodeMouseLeave({ config, nodeId: node.id });
+  }, [config, node.id]);
 
-  const compRef = React.useRef<HTMLElement>(null)
+  const compRef = React.useRef<HTMLElement>(null);
 
   // TODO: probably should add an observer to track node component size changes
   React.useLayoutEffect(() => {
-    const el = ReactDOM.findDOMNode(compRef.current) as HTMLInputElement
+    const el = ReactDOM.findDOMNode(compRef.current) as HTMLInputElement;
     if (el) {
       if (
         (node.size && node.size.width) !== el.offsetWidth ||
         (node.size && node.size.height) !== el.offsetHeight
       ) {
-        const newSize = { width: el.offsetWidth, height: el.offsetHeight }
-        setSize(newSize)
-        onNodeSizeChange({ config, nodeId: node.id, size: newSize })
+        const newSize = { width: el.offsetWidth, height: el.offsetHeight };
+        setSize(newSize);
+        onNodeSizeChange({ config, nodeId: node.id, size: newSize });
       }
     }
-  }, [node, compRef.current, size.width, size.height])
+  }, [node, compRef.current, size.width, size.height]);
 
   const children = (
     <>
       <ResizeObserver
         onResize={(rect) => {
-          const newSize = { width: rect.width, height: rect.height }
-          setSize(newSize)
+          const newSize = { width: rect.width, height: rect.height };
+          setSize(newSize);
         }}
       />
       <NodeInner node={node} config={config} />
       <Ports node={node} config={config}>
-        { Object.keys(node.ports).map((portId) => (
+        {Object.keys(node.ports).map((portId) => (
           <PortWrapper
             config={config}
             key={portId}
@@ -157,17 +184,17 @@ export const NodeWrapper = ({
             onLinkComplete={onLinkComplete}
             onLinkCancel={onLinkCancel}
           />
-        )) }
+        ))}
       </Ports>
     </>
-  )
+  );
 
   return (
     <Draggable
       bounds="parent"
       axis="both"
       position={node.position}
-      grid={[1,1]}
+      grid={[1, 1]}
       onStart={onStart}
       onDrag={onDrag}
       onStop={onStop}
@@ -179,11 +206,11 @@ export const NodeWrapper = ({
         children={children}
         onClick={onClick}
         onDoubleClick={onDoubleClick}
-        onMouseEnter={onMouseEnter}
+        onMouseEnter={() => {}} //onMouseEnter
         onMouseLeave={onMouseLeave}
         isSelected={isSelected}
         node={node}
       />
     </Draggable>
-  )
-}
+  );
+};
